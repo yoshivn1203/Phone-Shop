29914b8b6127433775670abd044e8dee55299da6
yoshi1203 <yoshivn1203@gmail.com>
2022-07-28 11:30:08 +0700

    back up data

--- customer/controller/phoneService.js
+++ customer/controller/phoneService.js
@@ -3,7 +3,7 @@ export class Service {
 
   getPhones = () => {
     return axios({
-      url: 'https://62dabe8ee56f6d82a768e4fc.mockapi.io/Phones',
+      url: 'https://run.mocky.io/v3/b2a54b78-e161-4e25-8287-180b4ac275ee',
       method: 'GET',
     });
   };
@@ -13,4 +13,16 @@ export class Service {
       method: 'GET',
     });
   };
+  // getPhones = () => {
+  //   return axios({
+  //     url: 'https://62dabe8ee56f6d82a768e4fc.mockapi.io/Phones',
+  //     method: 'GET',
+  //   });
+  // };
+  // getPhoneById = (id) => {
+  //   return axios({
+  //     url: `https://62dabe8ee56f6d82a768e4fc.mockapi.io/Phones/${id}`,
+  //     method: 'GET',
+  //   });
+  // };
 }
